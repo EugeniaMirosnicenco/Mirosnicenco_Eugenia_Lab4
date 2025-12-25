d7eeb640d98b701c95e77bed22e99bfa067ab88d
EugeniaMirosnicenco <miroshnicenco36@gmail.com>
2025-12-25 19:58:01 +0200

    Modificari Ex1

--- Mirosnicenco_Eugenia_Lab4/Controllers/PredictionController.cs
+++ Mirosnicenco_Eugenia_Lab4/Controllers/PredictionController.cs
@@ -6,8 +6,18 @@ namespace Mirosnicenco_Eugenia_Lab4.Controllers
 {
     public class PredictionController : Controller
     {
+        [HttpGet]
+        public IActionResult Price()
+        {
+            return View();
+        }
         public IActionResult Price(ModelInput input)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(input);
+            }
+
             // Load the model
             MLContext mlContext = new MLContext();
             // Create predection engine related to the loaded train model
